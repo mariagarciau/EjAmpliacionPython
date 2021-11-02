9ff35d9ffb0479864949a447d9cc33b3ca65ad24
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-11-02 11:40:02 +0100

    enunciado

--- ejSumaSimpleMatriz.py
+++ ejSumaSimpleMatriz.py
@@ -31,6 +31,18 @@ if __name__ == '__main__':
     fptr.write(str(result) + '\n')
     fptr.close()
 """
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
 def simpleArraySum(ar):
 
     if __name__ == '__main__':
