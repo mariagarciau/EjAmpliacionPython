bae26b4ea5b4b8249804e63ba1a4bf5974261fe3
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-11-06 20:04:36 +0100

    enunciado

--- ejEscalera.py
+++ ejEscalera.py
@@ -1,11 +1,10 @@
     """
-Esta es una escalera de tamaño n= 4:
+    Esta es una escalera de tamaño n= 4:
    #
   ##
  ###
 ####
-Su base y altura son iguales a n. Se dibuja mediante #símbolos y espacios.
-La última línea no está precedida por espacios.
+Su base y altura son iguales a n. Se dibuja mediante #símbolos y espacios. La última línea no está precedida por espacios.
 Escribe un programa que imprima una escalera de tamaño n .
 Función descriptiva
 Complete la función de staircase. staircase tiene los siguientes parámetros:
@@ -39,7 +38,7 @@ def staircase(n):
 if __name__ == '__main__':
     n = int(input().strip())
     staircase(n)
-"""
+    """
 #!/bin/python3
 import math
 import os
