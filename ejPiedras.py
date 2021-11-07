a5e7e2b51ffaf93cc1558ecaa763c77aa3b6e237
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-11-07 19:44:24 +0100

    enunciado

---
+++ ejPiedras.py
@@ -0,0 +1,12 @@
+"""Dos jugadores llamados P1 Y P2 están jugando un juego con un número inicial de n piedras.
+Jugador1 siempre juega primero, y los dos jugadores se mueven en turnos alternos.
+Las reglas del juego son las siguientes:
+En un solo movimiento, un jugador puede eliminar ,2,3 o 5 , o  piedras del tablero de juego.
+Si un jugador no puede hacer un movimiento, ese jugador pierde el juego.
+Dado el número inicial de piedras, busque e imprima el nombre del ganador.
+Cada jugador juega de manera óptima, lo que significa que no harán un movimiento que les haga perder
+el juego si existe un movimiento ganador.
+• Por ejemplo, si n=4, P1  puede hacer los siguientes movimientos:
+• P1 elimina 2 piedras quedando 2. P2 luego eliminará 2  piedras y ganar.
+• P1 elimina 3 piedras quedando 1. P2 no se puede mover y pierde.
+    """
